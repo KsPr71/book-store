310df1de8c2712317626260ec3ffb0f522ef3ac9
KSPR71 <jorgealejandrocasaresdelgado@gmail.com>
2025-11-07 15:01:06 -0500

    logoSplash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [
       { url: "/icons/icon-180x180.png", sizes: "180x180", type: "image/png" },
